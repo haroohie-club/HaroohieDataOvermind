1cd4c6aeea39c828a69815851c05f27802aaac89
jonko0493 <jonko0493@protonmail.com>
2025-09-01 04:58:49 -0700

    Back to the lab again

--- HaroohieDataOvermind/Program.cs
+++ HaroohieDataOvermind/Program.cs
@@ -44,7 +44,6 @@ public class Program
             ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
         });
 
-
         MongoClientSettings clientSettings = new()
         {
             Scheme = ConnectionStringScheme.MongoDB, 
